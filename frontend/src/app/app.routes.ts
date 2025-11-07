cd0f92975c25f1b86df844d01d43a79f6dd807c0
lucas-morim <lucasmorim05@gmail.com>
2025-11-07 13:33:34 +0000

    creating new organization folders (frontoffice/backoffice)

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,5 +1,5 @@
 import { Routes } from '@angular/router';
-import { Home } from './pages/home/home';
+import { Home } from './pages/frontoffice/home/home';
 
 export const routes: Routes = [
 { path: '', component: Home},
