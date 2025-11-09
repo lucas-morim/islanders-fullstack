570426fd35d6bf4381371f9c7634273ea00925d2
lucas-morim <lucasmorim05@gmail.com>
2025-11-09 19:16:29 +0000

    dashboard base structure

--- frontend/src/app/components/backoffice/header/header.ts
+++ frontend/src/app/components/backoffice/header/header.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { DOCUMENT } from '@angular/common';
+import { Renderer2 } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-header',
-  imports: [],
+  standalone: true,
+  selector: 'app-backoffice-header',
+  imports: [RouterLink, CommonModule],
   templateUrl: './header.html',
-  styleUrl: './header.css',
+  styleUrls: ['./header.css'],
 })
 export class Header {
+  notifications = 5;
+  tasks = 5;
+  emails = 5;
 
 }
--- frontend/src/app/pages/backoffice/backoffice.routes.ts
+++ frontend/src/app/pages/backoffice/backoffice.routes.ts
@@ -1,18 +1,17 @@
-// src/app/pages/backoffice/backoffice.routes.ts
 import { Routes } from '@angular/router';
-import { BackShell } from '../../layouts/back-shell/back-shell';
-import { Dashboard } from './dashboard/dashboard';
 
 export const routes: Routes = [
   {
     path: '',
-    component: BackShell,
-    //canActivate: [adminGuard],     
+    loadComponent: () =>
+      import('../../layouts/back-shell/back-shell').then(c => c.BackShell),
     children: [
-      { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
-      { path: 'dashboard', component: Dashboard },
-      // futuros CRUDs:
-      // { path: 'users', loadComponent: () => import('./users/users.component').then(m => m.UsersComponent) },
-    ]
-  }
+      { path: '', pathMatch: 'full', redirectTo: 'dashboard' },
+      {
+        path: 'dashboard',
+        loadComponent: () =>
+          import('./dashboard/dashboard').then(c => c.Dashboard),
+      },
+    ],
+  },
 ];
