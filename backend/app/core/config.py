805f1cb6fd982cfa0ae541b84a6ea28d0bfa2156
Gonçalo Oliveira <goliveira2212004@gmail.com>
2025-11-19 21:22:27 +0000

    config atualizado para aceitar jwt  e main e init atualizados para aceitarem "auth"

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,15 +1,17 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
-    ENV: str = "dev"
+    ENV: str = "dev" 
+
+    # API
     API_PREFIX: str = "/api/v1"
     FRONTEND_ORIGIN: str = "http://localhost:4200"
-    DATABASE_URL: str  
+    DATABASE_URL: str 
 
-    SECRET_KEY: str  # JWT secret
+    # JWT
+    JWT_SECRET: str
     JWT_ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60
-    REFRESH_TOKEN_EXPIRE_DAYS: int = 7
 
     model_config = SettingsConfigDict(
         env_file=".env",
@@ -17,4 +19,6 @@ class Settings(BaseSettings):
         case_sensitive=False,
     )
 
+# Outros arquivos podem importar assim:
+# from app.core.config import settings
 settings = Settings()
