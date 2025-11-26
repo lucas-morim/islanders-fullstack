ddd1bd05aed0c09438a1cfd72dddda150508cdbb
Gonçalo Oliveira <goliveira2212004@gmail.com>
2025-11-26 23:25:20 +0000

    some changes in some IDs in HTML and typescript for link the API with backend

--- frontend/src/app/pages/frontoffice/register/register.ts
+++ frontend/src/app/pages/frontoffice/register/register.ts
@@ -1,13 +1,35 @@
 import { Component } from '@angular/core';
-import { RouterLink } from "@angular/router";
+import { RouterLink, Router } from "@angular/router";
+import { FormsModule } from '@angular/forms';
+import { AuthService } from "../auth/auth.service";
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, FormsModule], // IMPORTANTE para ngModel
   templateUrl: './register.html',
-  styleUrl: './register.css',
+  styleUrls: ['./register.css'],
 })
 export class Register {
+  form = { name: '', email: '', username: '', password: '', passwordConfirm: '' };
 
+  constructor(private auth: AuthService, private router: Router) {}
+
+  async submitRegister(event: Event) {
+    event.preventDefault();
+
+    if (this.form.password !== this.form.passwordConfirm) {
+      alert("As senhas não coincidem!");
+      return;
+    }
+
+    try {
+      const { access_token } = await this.auth.register(this.form);
+      localStorage.setItem('access_token', access_token);
+      await this.router.navigate(['/']);
+    } catch (e) {
+      console.error(e);
+      alert("Erro ao criar conta");
+    }
+  }
 }
