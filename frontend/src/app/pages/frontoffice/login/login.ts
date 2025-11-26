4aa4d240fc29bc32c03f7b9cf6cf9a1ba34cf684
Gonçalo Oliveira <goliveira2212004@gmail.com>
2025-11-26 23:23:40 +0000

    login auth (html and typescript) with link to get the token when submit done

--- frontend/src/app/pages/frontoffice/login/login.ts
+++ frontend/src/app/pages/frontoffice/login/login.ts
@@ -1,13 +1,30 @@
-import { Component } from '@angular/core';
-import { RouterLink } from "@angular/router";
+import { Component, inject } from '@angular/core';
+import { Router, RouterLink } from "@angular/router";
+import { AuthService } from "../auth/auth.service";
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, FormsModule],
   templateUrl: './login.html',
-  styleUrl: './login.css',
+  styleUrls: ['./login.css'],
 })
 export class Login {
+  private auth = inject(AuthService);
+  private router = inject(Router);
 
+  form = { username: '', password: '' };
+
+  async submitLogin(event: Event) {
+    event.preventDefault();
+    try {
+      const token = await this.auth.login(this.form);
+      localStorage.setItem('access_token', token.access_token);
+      await this.router.navigate(['/']);
+    } catch (e) {
+      console.error(e);
+      alert("Credenciais inválidas");
+    }
+  }
 }
