4cc882b11499b525458cb91120556ed03760ba1d
Gonçalo Oliveira <goliveira2212004@gmail.com>
2025-11-26 23:39:54 +0000

    some changes in header for have an avatar with dropdown. .ts for check if user is logged in or not

--- frontend/src/app/components/frontoffice/header/header.ts
+++ frontend/src/app/components/frontoffice/header/header.ts
@@ -1,27 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { Router, NavigationEnd } from '@angular/router';
 import { filter } from 'rxjs';
 import { RouterLink } from '@angular/router';
+import { NgIf } from '@angular/common';
+import { AuthState } from '../../../pages/frontoffice/auth/auth.state';
 
 @Component({
   selector: 'frontoffice-header',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, NgIf],
   templateUrl: './header.html',
   styleUrls: ['./header.css'],
 })
 export class Header {
+
   logo: string = 'assets/islaverse.png';
+  auth = inject(AuthState);
 
   constructor(private router: Router) {
     this.router.events
       .pipe(filter(event => event instanceof NavigationEnd))
       .subscribe((event: NavigationEnd) => {
-        if (event.url.startsWith('/about')) {
-          this.logo = 'assets/islaverse2.png';
-        } else {
-          this.logo = 'assets/islaverse.png';
-        }
+        this.logo = event.url.startsWith('/about')
+          ? 'assets/islaverse2.png'
+          : 'assets/islaverse.png';
       });
   }
+
+  logout() {
+    this.auth.logout();
+    this.router.navigate(['/']);
+  }
+
 }
