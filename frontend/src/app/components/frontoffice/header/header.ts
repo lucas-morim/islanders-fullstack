16f4cd1ec5a19b86823e6a64b5ad72b9a425e3be
Gonçalo Oliveira <goliveira2212004@gmail.com>
2025-11-27 18:39:22 +0000

    header updated with photo

--- frontend/src/app/components/frontoffice/header/header.ts
+++ frontend/src/app/components/frontoffice/header/header.ts
@@ -12,6 +12,7 @@ import { AuthState } from '../../../pages/frontoffice/auth/auth.state';
   templateUrl: './header.html',
   styleUrls: ['./header.css'],
 })
+
 export class Header {
   auth = inject(AuthState);
   logo: string = 'assets/islaverse.png';
@@ -28,6 +29,12 @@ export class Header {
         this.dropdownOpen.set(false); // fecha dropdown ao navegar
       });
   }
+
+  get userPhoto(): string {
+  const photo = this.auth.user()?.photo;
+  return photo ? 'http://127.0.0.1:8000' + photo : 'assets/avatar-default.png';
+  }
+
 
   toggleDropdown() {
     this.dropdownOpen.set(!this.dropdownOpen());
