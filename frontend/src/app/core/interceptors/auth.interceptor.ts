cb95f101034b834ef6024c61385a03e838b9fca3
Gonçalo Oliveira <goliveira2212004@gmail.com>
2025-12-01 17:05:05 +0000

    changes in auth interceptor for dont need more manual headers

--- frontend/src/app/core/interceptors/auth.interceptor.ts
+++ frontend/src/app/core/interceptors/auth.interceptor.ts
@@ -1,28 +1,18 @@
-import { Injectable, inject } from '@angular/core';
-import {
-  HttpInterceptor,
-  HttpRequest,
-  HttpHandler,
-  HttpEvent,
-} from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
 import { AuthState } from '../../pages/frontoffice/auth/auth.state';
 
-@Injectable()
-export class AuthInterceptor implements HttpInterceptor {
-  private authState = inject(AuthState);
+export const authInterceptor: HttpInterceptorFn = (req, next) => {
+  const auth = inject(AuthState);
+  const token = auth.access_token();
 
-  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    const token = this.authState.access_token();
-
-    if (token) {
-      req = req.clone({
-        setHeaders: {
-          Authorization: `Bearer ${token}`,
-        },
-      });
-    }
-
-    return next.handle(req);
+  if (token) {
+    req = req.clone({
+      setHeaders: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
   }
-}
+
+  return next(req);
+};
