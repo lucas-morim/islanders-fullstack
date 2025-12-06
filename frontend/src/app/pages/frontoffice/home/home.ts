a62aead6fb6223d6f37c96fe9ad5351d40e01814
Rúben Teixeira <rubenfteixeira@gmail.com>
2025-12-06 21:31:35 +0000

    doing some cool stuff with my cards

--- frontend/src/app/pages/frontoffice/home/home.ts
+++ frontend/src/app/pages/frontoffice/home/home.ts
@@ -1,13 +1,61 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, signal, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { CourseService, CourseOut } from '../../backoffice/course/course.service';
+import { NgFor } from '@angular/common';
+
+interface CourseCard {
+  id: string;
+  name: string;
+  description: string;
+  ects: number;
+  hours: number;
+  price: number;
+  image: string;
+  link: string;
+}
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, NgFor],
   templateUrl: './home.html',
-  styleUrls: ['./home.css',]
+  styleUrls: ['./home.css']
 })
-export class Home {
+export class Home implements OnInit {
+  private coursesSvc = inject(CourseService);
 
+  loading = signal(false);
+  courses = signal<CourseCard[]>([]);
+
+  async ngOnInit() {
+    this.loading.set(true);
+
+    try {
+
+      const data: CourseOut[] = await this.coursesSvc.list(0, 100);
+
+      const BACKEND_URL = 'http://127.0.0.1:8000';
+
+
+
+      const cards: CourseCard[] = data.map(c => ({
+        id: c.id,
+        name: c.title,
+        description: c.description ?? '',
+        ects: c.credits ?? 0,
+        hours: c.num_hours ?? 0,
+        price: c.price ?? 0,
+        image: `${BACKEND_URL}${c.photo}`,
+        link: `/curso/${c.id}`,
+      }));
+
+      this.courses.set(cards);
+
+      console.log('Cursos carregados da API:', this.courses());
+    } catch (err) {
+      console.error('Erro ao carregar cursos:', err);
+    } finally {
+      this.loading.set(false);
+    }
+  }
 }
