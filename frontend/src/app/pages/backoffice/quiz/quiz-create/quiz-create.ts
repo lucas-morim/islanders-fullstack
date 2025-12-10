8f3105b438669708e9891e9f5cc50e32c4e8141d
lucas-morim <lucasmorim05@gmail.com>
2025-12-10 10:39:30 +0000

    creating quiz pages

---
+++ frontend/src/app/pages/backoffice/quiz/quiz-create/quiz-create.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-quiz-create',
+  imports: [],
+  templateUrl: './quiz-create.html',
+  styleUrl: './quiz-create.css',
+})
+export class QuizCreate {
+
+}
